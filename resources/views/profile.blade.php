a55ec067f2c36315a598aad9e4b7fbe7d2887c4d
nguyencongluat21092001 <luatncth2007024@fpt.edu.vn>
2021-08-04 00:21:13 +0700

    creat folder laravel to github

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Welcome Quang Bede đến với laravel</h1>
+        <h2>Quang có ny Là: {{$name}}</h2>
+    </body>
+</html>
+
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+Route::get('/','HomeController@showWelcome')
+
+// use Illuminate\Support\Facades\Redirect;
+// Route ::get('/home','HomeController@showWelcome');
+// Route::get('/', function () {
+//     return view('hello');
+//  });
+// Route::get('profile/{name}','ProfileController@showProfile');
+
+// Route ::get('home','HomeController@showWelcome');
+// //Route ::get('about','AboutController@showDetails');
+
+
+
+// Route ::get('about',function(){
+//     return 'About Conten';
+// });
+// Route ::get ('about/{theSubject}','AboutController@showSubject');
+
+// Route ::get('about/directions',function(){
+//     return 'Conten directions';
+// });
+// Route ::get('submit-form',function(){
+//     return 'Process Form';
+// });
+// Route ::get('about/{thesubject}',function($thesubject){
+//     return $thesubject.' Conten';
+// });
+// Route ::get('about/{Price}/{art}',function($Price,$art){
+//     return "the product: $Price and $art";
+
+// });
+// // uy quyen chuyen trang
+// Route::get('where',function(){
+//     return Redirect :: to ('about/directions');
+// });
