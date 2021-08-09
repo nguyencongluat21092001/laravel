05a48133b8727dd8ac6b58613d58281a71507a7a
nguyencongluat21092001 <luatncth2007024@fpt.edu.vn>
2021-08-10 05:06:20 +0700

    luat

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-Route::get('/','HomeController@showWelcome');
+Route::get('/home','HomeController@showWelcome');
 // pull git to visua
 // ok update;
 
